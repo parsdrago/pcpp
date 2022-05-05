a488b89050f2306ac6effd6d06198224acb3dfeb
utan <iw.utan@gmail.com>
2022-05-05 18:54:04 +0900

    tokenize digits

--- py2c/py2c.py
+++ py2c/py2c.py
@@ -7,8 +7,32 @@ int main(void) {
 }"""
 
 
+def tokenize(code):
+    tokens = []
+    i = 0
+    while i < len(code):
+        c = code[i]
+        if c.isdigit():
+            current = int(c)
+            i += 1
+            while i < len(code) and code[i].isdigit():
+                c = code[i]
+                if c.isdigit():
+                    current = current * 10 + int(c)
+                else:
+                    break
+                i += 1
+            i -= 1
+            tokens.append(current)
+        i += 1
+
+    return tokens
+
+
 def output_integer(i):
-    print(TEMPLATE.replace("{{return value}}", i))
+    tokens = tokenize(i)
+    if len(tokens) > 0:
+        print(TEMPLATE.replace("{{return value}}", str(tokens[-1])))
 
 
 if __name__ == "__main__":
--- tests/test_py2c.py
+++ tests/test_py2c.py
@@ -1,5 +1,18 @@
 from py2c import __version__
+from py2c import py2c
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_tokenize_1():
+    assert py2c.tokenize("1") == [1]
+
+
+def test_tokenize_42():
+    assert py2c.tokenize("42") == [42]
+
+
+def test_tokenize_two_digits():
+    assert py2c.tokenize("42 12") == [42, 12]
