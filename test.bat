08bc100ca1a2b54b1403455b833ec4cbb3dc937b
utan <iw.utan@gmail.com>
2022-05-10 23:18:30 +0900

    add test for recursive funcion call

--- test.bat
+++ test.bat
@@ -14,6 +14,7 @@ call:test_script test8.py 42
 call:test_script test9.py 42
 call:test_script test10.py 42
 call:test_script test11.py 42
+call:test_script test12.py 144
 call:testcase "return 12" 12
 call:testcase "return 42" 42
 call:testcase "return 1+2" 3
